bbb9ead0eaee9b315b3ea043d721d8d749b867ab
DafaaaYxz <zeuz8686@gmail.com>
2025-12-24 11:19:17 +0700

    Update setting.js

--- setting.js
+++ setting.js
@@ -2,6 +2,6 @@
 export const config = {
     bot_token: "8401653057:AAFKAkoGufOGBuO0GidtaSMO8DZiS7yKomA",
     owner_id: 7341190291,
-    uptash_url: "https://growing-firefly-50232.upstash.io", // Contoh: https://nama-db.upstash.io
+    uptash_url: "https://growing-firefly-50232.upstash.io",
     uptash_token: "AcQ4AAIncDFlYjI2ZWM2ODhmOGQ0N2YwOTI1Njg5ZDA3ZjRjMDdhMHAxNTAyMzI"
 };
