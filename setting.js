f3aebb93b7eb62e3015afc1a0690ea471cc464b3
DafaaaYxz <zeuz8686@gmail.com>
2025-12-24 11:04:31 +0700

    Update setting.js

--- setting.js
+++ setting.js
@@ -1,6 +1,6 @@
 
 export const config = {
-    bot_token: "ISI_TOKEN_BOT_TELEGRAM_DISINI",
+    bot_token: "8401653057:AAFKAkoGufOGBuO0GidtaSMO8DZiS7yKomA",
     owner_id: 7341190291,
     uptash_url: "ISI_UPSTASH_REST_URL_DISINI", // Contoh: https://nama-db.upstash.io
     uptash_token: "ISI_UPSTASH_REST_TOKEN_DISINI"
